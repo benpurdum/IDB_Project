7b1389603617029aa9c4625c9e86422613bd9e10
zeigenbrod <zoeeig161@gmail.com>
2026-04-08 12:37:10 -0400

    working on storedProcedures

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -17,20 +17,45 @@ END
 --Instructor Read
 
 CREATE PROCEDURE findInstructor (
-    In pID VARCHAR(5)
+    IN pID VARCHAR(5)
 )
 BEGIN
-    select from instructor
-    where instructor.ID = pID;
+    SELECT * FROM INSTRUCTOR
+    WHERE instructor.ID = pID;
 END
 
 --Instructor Update
 
-CREATE PROCEDURE updateInstructorName ()
+CREATE PROCEDURE updateInstructorName (
+    IN pID VARCHAR(5),
+    IN pName VARCHAR(20)
+)
+BEGIN
+    UPDATE instructor
+    SET name = pName
+    WHERE instructor.ID = pID;
+END
 
-CREATE PROCEDURE updateInstructorSalary ()
+CREATE PROCEDURE updateInstructorSalary (
+    IN pID VARCHAR(5),
+    IN pSalary DECIMAL(8,2)
+)
+BEGIN
+    UPDATE instructor
+    SET salary = pSalary
+    WHERE instructor.ID = pID;
+END
 
-CREATE PROCEDURE updateInstructorDept ()
+CREATE PROCEDURE updateInstructorDept (
+    IN pID VARCHAR(5),
+    IN pDept_name VARCHAR(20)
+)
+BEGIN
+    UPDATE Instructor
+    SET dept_name = pDept_name
+    WHERE instructor.ID = pID;
+END
+
 
 
 --Instructor Delete
@@ -65,26 +90,41 @@ CREATE PROCEDURE findStudent (
     In pID VARCHAR(5)
 )
 BEGIN
-    select from instructor
+    SELECT * FROM instructor
     where student.ID = pID;
 END
 
 --Student Update
 
-CREATE PROCEDURE updateStudentName ()
+CREATE PROCEDURE updateStudentName (
+    IN pID VARCHAR(5),
+    IN pName VARCHAR(20)
+)
+BEGIN
+    UPDATE student
+    SET name = pName
+    WHERE student.ID = pID;
+END
 
-CREATE PROCEDURE updateStudentDept ()
+CREATE PROCEDURE updateStudentDept (
+    IN pID VARCHAR(5),
+    IN pDept_name VARCHAR(20)
+)
+BEGIN
+    UPDATE student
+    SET dept_name = pDept_name
+    WHERE student.ID = pID;
+END
 
-CREATE PROCEDURE updateStudentCredit ()
 
 --Student Delete
 
 CREATE PROCEDURE deleteStudent (
-    In pID VARCHAR(5)
+    IN pID VARCHAR(5)
 )
 BEGIN
-    delete from student
-    where student.ID = pID;
+    DELETE FROM student
+    WHERE student.ID = pID;
 END
 
 --SECTION CRUD
@@ -107,17 +147,48 @@ BEGIN
 END
 
 
---Section Read
+--Section Read (Find all sections of a class)
 
-CREATE PROCEDURE findSection ()
+CREATE PROCEDURE findSectionsOfClass (
+    IN pCourse_id VARCHAR(8)
+)
+BEGIN
+    SELECT * FROM section
+    WHERE section.course_id = pCourse_id;
+END
 
 --Section Update
 
-CREATE PROCEDURE updateSectionBuilding ()
+CREATE PROCEDURE updateSectionBuilding (
+    IN pSec_id VARCHAR(8),
+    IN pBuilding VARCHAR(15)
+)
+BEGIN
+    UPDATE section
+    SET building = pBuilding
+    WHERE section.sec_id = pSec_id;
+END
 
-CREATE PROCEDURE updateSectionRoomNumber ()
+CREATE PROCEDURE updateSectionRoomNumber (
+    IN pSec_id VARCHAR(8),
+    IN pRoom_number VARCHAR(7)
+)
+BEGIN
+    UPDATE section
+    SET room_number = pRoom_number
+    WHERE section.sec_id = pSec_id;
+END
 
-CREATE PROCEDURE updateTimeSlotId ()
+
+CREATE PROCEDURE updateTimeSlotId (
+    IN pSec_id VARCHAR(8),
+    IN pTime_slot_id VARCHAR(4)
+)
+BEGIN
+    UPDATE section
+    SET time_slot_id = pTime_slot_id
+    WHERE section.sec_id = pSec_id;
+END
 
 --Section Delete
 
@@ -131,16 +202,67 @@ END
 
 --Enroll in a Class
 
-CREATE PROCEDURE classEnroll ()
+CREATE PROCEDURE classEnroll (
+    IN pStudent_id VARCHAR(5),
+    IN pCourse_id VARCHAR(8),
+    IN pSec_id VARCHAR(8),
+    IN pSemester VARCHAR(6),
+    IN pYear DECIMAL(4,0)
+)
+BEGIN
+    INSERT INTO takes (ID, course_id, sec_id, semester, year)
+    VALUES (pStudent_id, pCourse_id, pSec_id, pSemester, pYear);
+END
 
 --Assign Instructor to Class
 
-CREATE PROCEDURE assignInstructor ()
+CREATE PROCEDURE assignInstructor (
+    IN pInstructor_id VARCHAR(5),
+    IN pCourse_id VARCHAR(8),
+    IN pSec_id VARCHAR(8),
+    IN pSemester VARCHAR(6),
+    IN pYear DECIMAL(4,0)
+)
+BEGIN
+    INSERT INTO teaches (ID, course_id, sec_id, semester, year)
+    VALUES (pInstructor_id, pCourse_id, pSec_id, pSemester, pYear);
+END
 
---Drop a Section
 
-CREATE PROCEDURE dropSection ()
+--Drop a Section for a student
 
---Give a Grade to a Section
+CREATE PROCEDURE dropSection (
+    IN pStudent_id VARCHAR(5),
+    IN pCourse_id VARCHAR(8),
+    IN pSec_id VARCHAR(8),
+    IN pSemester VARCHAR(6),
+    IN pYear DECIMAL(4,0)
+)
+BEGIN
+    DELETE FROM takes
+    WHERE ID = pStudent_id
+    AND course_id = pCourse_id
+    AND sec_id = pSec_id
+    AND semester = pSemester
+    AND year = pYear;
+END
 
-CREATE PROCEDURE gradeSection ()
+--Give a Grade to a Section for a student
+
+CREATE PROCEDURE gradeSection (
+    IN pStudent_id VARCHAR(5),
+    IN pCourse_id VARCHAR(8),
+    IN pSec_id VARCHAR(8),
+    IN pSemester VARCHAR(6),
+    IN pYear DECIMAL(4,0),
+    IN pGrade VARCHAR(2)
+)
+BEGIN
+    UPDATE takes
+    SET grade = pGrade
+    WHERE ID = pStudent_id
+    AND course_id = pCourse_id
+    AND sec_id = pSec_id
+    AND semester = pSemester
+    AND year = pYear;
+END
